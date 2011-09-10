640fc8b235bc05ccf9fbd7317e4a548cd4970426
Nathan DeGrand <degrand1@illinois.edu>
2011-09-09 23:23:42 -0500

    First week demo

---
+++ current/Demo1/Player.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Demo1
+{
+    class Player : Sprite
+    {
+        const int PLAYER_SPEED = 150;
+        const String theAssetName = "filler sheet";
+        const int MOVE_UP = -1;
+        const int MOVE_DOWN = 1;
+        const int MOVE_RIGHT = 1;
+        const int MOVE_LEFT = -1;
+        const int SPRITE_WIDTH = 128;
+        const int SPRITE_HEIGHT = 128;
+        enum State
+        {
+            Walking
+        }
+        State currentState = State.Walking;
+        Vector2 mSpeed = Vector2.Zero;
+        Vector2 mDirection = Vector2.Zero;
+
+        KeyboardState mPreviousKeyboardState;
+
+        private void UpdateMovement(KeyboardState currentKeyboardState)
+        {
+            if (currentState == State.Walking)
+            {
+                if (currentKeyboardState.IsKeyDown(Keys.Right))
+                {
+                    if (mDirection.X != MOVE_RIGHT)
+                    {
+                        source = new Rectangle(0, 2 * SPRITE_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT);
+                    }
+                    mSpeed.X = PLAYER_SPEED;
+                    mDirection.X = MOVE_RIGHT;
+                }
+                else if (currentKeyboardState.IsKeyDown(Keys.Left))
+                {
+                    if (mDirection.X != MOVE_LEFT)
+                    {
+                        source = new Rectangle(0, SPRITE_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT);
+                    }
+                    mSpeed.X = PLAYER_SPEED;
+                    mDirection.X = MOVE_LEFT;
+                }
+                else
+                {
+                    if (mDirection != Vector2.Zero)
+                    {
+                        source = new Rectangle(0, 0, SPRITE_WIDTH, SPRITE_HEIGHT);
+                    }
+                    mSpeed = Vector2.Zero;
+                    mDirection = Vector2.Zero;
+                }
+            }
+        }
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState currentKeyboardState = Keyboard.GetState();
+            UpdateMovement(currentKeyboardState);
+            mPreviousKeyboardState = currentKeyboardState;
+            base.Update(gameTime, mSpeed, mDirection);
+        }
+
+    }
+}
---
+++ current/Demo1/Sprite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Demo1
+{
+    class Sprite
+    {
+        public Vector2 position = new Vector2(0, 0);
+        public Rectangle source = new Rectangle(0, 0, 128, 128);
+        private Texture2D texture;
+        
+        //Load the texture for the sprite using the Content Pipeline
+        public void LoadContent(ContentManager theContentManager, string assetName)
+        {
+            texture = theContentManager.Load<Texture2D>(assetName);
+        }
+        //Draw a sprite to the screen
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, source, Color.White);
+        }
+        //Update the sprite
+        public void Update(GameTime gameTime, Vector2 speed, Vector2 direction)
+        {
+            position += speed * direction * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
